5b572fef0498f7e5e7706777af0321ecb4131bea
Aleksandar Todorov <aleks.todorov11@gmail.com>
2025-10-24 14:03:28 +0300

    Update page.tsx

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -20,6 +20,14 @@ export default function Home() {
           <Conversions />
         </Container>
       </div>
+      <div className="grid grid-cols-1 divide-y border-b border-border laptop:grid-cols-3 laptop:divide-x laptop:divide-y-0 laptop:divide-border">
+        <Container className="py-4 laptop:col-span-2">
+          <AverageTicketsCreated />
+        </Container>
+        <Container className="py-4 laptop:col-span-1">
+          <Conversions />
+        </Container>
+      </div>
       <div className="grid grid-cols-1 divide-y border-b border-border laptop:grid-cols-2 laptop:divide-x laptop:divide-y-0 laptop:divide-border">
         <Container className="py-4 laptop:col-span-1">
           <TicketByChannels />
